a2a6be7385c56c895cb1e84cffdaff063086fe6e
TheKiller65YT <jackgonser@gmail.con>
2020-09-06 22:54:19 -1000

    Update Unit Tests

--- src/test/java/me/physicsarebad/warps/guis/MainGUITest.java
+++ src/test/java/me/physicsarebad/warps/guis/MainGUITest.java
@@ -8,12 +8,12 @@ public class MainGUITest {
     @Test
     public void testGUIName() {
         //Public Warps
-        assertEquals("Failed Public Warps name test", "Public Warps", MainGUI.WarpType.PUBLIC.name+" Warps");
+        assertEquals("Failed Public Warps name test", "Public Warps", MainGUI.WarpType.PUBLIC.name+"Warps");
 
         //Private Warps
-        assertEquals("Failed Private Warps name test", "Private Warps", MainGUI.WarpType.PRIVATE.name+" Warps");
+        assertEquals("Failed Private Warps name test", "Private Warps", MainGUI.WarpType.PRIVATE.name+"Warps");
 
         //Server Warps
-        assertEquals("Failed Server Warps name test", "Server Warps", MainGUI.WarpType.PUBLIC.name+" Warps");
+        assertEquals("Failed Server Warps name test", "Server Warps", MainGUI.WarpType.SERVER.name+"Warps");
     }
 }
